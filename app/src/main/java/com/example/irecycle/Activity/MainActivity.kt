a3bdb526e7cdd4dfcae2f799cefac86247cdd2ab
Kunal <kunalmehrotra2001@gmail.com>
2022-06-18 13:04:13 +0530

    added first fragment

--- app/src/main/java/com/example/irecycle/Activity/MainActivity.kt
+++ app/src/main/java/com/example/irecycle/Activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.irecycle
+package com.example.irecycle.Activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.irecycle.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
